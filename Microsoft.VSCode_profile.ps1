55e83019a0c0a9c2aa7082d64b3c1e41c9840d32
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-10-10 20:26:07 -0700

    Adds conditional sourcing of PowerShell profile for VSCode

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -2,5 +2,18 @@
 Write-Host "Loading profile..."
 
 # Dot-source the script containing custom functions
-. "C:\Users\jayha\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+# . "C:\Users\jayha\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 
+if ($IsMacOS) {
+  Write-Host "PowerShell is running on macOS."
+  . "/Users/jaycrawford/.config/powershell/Microsoft.PowerShell_profile.ps1"
+}
+elseif ($IsLinux) {
+  Write-Host "PowerShell is running on Linux. Configure your PowerShell profile"
+}
+elseif ($IsWindows) {
+  . "C:\Users\jayha\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+}
+else {
+  Write-Host "PowerShell is running on an unknown operating system."
+}
